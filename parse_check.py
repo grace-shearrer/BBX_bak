7978fda287b4fe8c52016273f6678f23440d7e44
Grace-Shearrer <grace.shearrer@gmail.com>
2019-12-11 17:16:58 -0500

    finished

--- parse_check.py
+++ parse_check.py
@@ -16,6 +16,55 @@ import glob
 import pdb
 import argparse
 import pandas as pd
+
+def plottin(df, arglist):
+    import matplotlib
+    # matplotlib.use("Qt5Agg")
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    dates = df['onsets']
+    names = df['metric']
+    # Choose some nice levels
+    levels = np.tile([-5, 5, -3, 3, -1, 1],
+                     int(np.ceil(len(dates)/6)))[:len(dates)]
+
+    # Create figure and plot a stem plot with the date
+    fig, ax = plt.subplots(figsize=(25, 4), constrained_layout=True)
+    ax.set(title="Matplotlib release dates")
+
+    markerline, stemline, baseline = ax.stem(dates, levels,
+                                             linefmt="C3-", basefmt="k-",
+                                             use_line_collection=True)
+
+    plt.setp(markerline, mec="k", mfc="w", zorder=3)
+
+    # Shift the markers to the baseline by replacing the y-data by zeros.
+    markerline.set_ydata(np.zeros(len(dates)))
+
+    # annotate lines
+    vert = np.array(['top', 'bottom'])[(levels > 0).astype(int)]
+    for d, l, r, va in zip(dates, levels, names, vert):
+        ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l)*3),textcoords="offset points",
+                     va=va, ha="center")
+
+    # format xaxis with 4 month intervals
+    # ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=4))
+    # ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%b %Y"))
+    plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+
+    # remove y axis and spines
+    ax.get_yaxis().set_visible(False)
+    for spine in ["left", "top", "right"]:
+        ax.spines[spine].set_visible(False)
+
+    ax.margins(y=0.1)
+    # plt.show()
+    plt.savefig(os.path.join(arglist['OUTPATH'],'%s_%s.png'%(arglist['sub'],arglist['run'])))
+
+
+
+
 def parsely(arglist):
     # print(arglist)
     ignore = ['DATA 	Keypress: o']
@@ -28,6 +77,8 @@ def parsely(arglist):
         sub=fi.split('_')[1]
         run=file.split('_')[2].split('0')[-1]
         session=arglist['WAVE']
+        arglist.update({'sub':sub})
+        arglist.update({'run':run})
         print('this is the subject %s\nthis is the run %s'%(sub,run))
 
         with open(file,'r') as infile:
@@ -61,6 +112,7 @@ def parsely(arglist):
                     if x.find('Level RINSE 	25')>-1:
                         rinse.append(l_s[0])
         print("got the arrays time to adjust onsets")
+        check_dict={}
         columnTitles = ['onsets', 'duration', 'metric']
         # CUES
         cue_df = pd.DataFrame(
@@ -73,17 +125,28 @@ def parsely(arglist):
         ## make individual dataframes
         ### H2O cue
         H2Ocue_df=cue_df.loc[cue_df['metric'] == 'image=water.jpg']
+        x=cue_df.loc[cue_df['metric'] == 'image=water.jpg'] #this is just for the dict and I don't know why
+        #### check dataframe
+        check_dict.update({'H2Ocue':x})
+        ####################
         H2Ocue_df['metric'].replace({'image=water.jpg': 1}, inplace=True)
-        # sub-001_task-H2Ocue_run-1
         H2Ocue_df.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-H2Ocue-run-%s.tsv'%(sub,run)), header=False, index=False, sep='\t')
         ### SSB cue
         SSBcue = ['image=CO.jpg','image=SL.jpg']
         SSBcue_df=cue_df.loc[(cue_df['metric'] == SSBcue[0]) | (cue_df['metric'] == SSBcue[1])]
+        #### check dataframe
+        y=cue_df.loc[(cue_df['metric'] == SSBcue[0]) | (cue_df['metric'] == SSBcue[1])]
+        check_dict.update({'SSBcue':y})
+        ####################
         SSBcue_df['metric'].replace({SSBcue[0]: 1, SSBcue[1]: 1}, inplace=True)
         SSBcue_df.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-SSBcue-run-%s.tsv'%(sub,run)), header=False, index=False, sep='\t')
         ### USB cue
         USBcue = ['image=UCO.jpg','image=USL.jpg']
         USBcue_df=cue_df.loc[(cue_df['metric'] == USBcue[0]) | (cue_df['metric'] == USBcue[1])]
+        #### check dataframe
+        z=cue_df.loc[(cue_df['metric'] == USBcue[0]) | (cue_df['metric'] == USBcue[1])]
+        check_dict.update({'USBcue':z})
+        ###################
         USBcue_df['metric'].replace({USBcue[0]: 1, USBcue[1]: 1}, inplace=True)
         USBcue_df.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-USBcue-run-%s.tsv'%(sub,run)), header=False, index=False, sep='\t')
         # TASTES
@@ -97,14 +160,26 @@ def parsely(arglist):
         ## make individual dataframes
         ### H2O
         H2O_df=taste_df.loc[taste_df['metric'] == '0']
+        #### check dataframe
+        a=taste_df.loc[taste_df['metric'] == '0']
+        check_dict.update({'H2O':a})
+        ###################
         H2O_df['metric'].replace({'0': 1}, inplace=True)
         H2O_df.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-H2O-run-%s.tsv'%(sub,run)), header=False, index=False, sep='\t')
         ## SSB
         SSB_df=taste_df.loc[taste_df['metric'] == '1']
+        #### check dataframe
+        b=taste_df.loc[taste_df['metric'] == '1']
+        check_dict.update({'SSB':b})
+        ####################
         SSB_df['metric'].replace({'1': 1}, inplace=True)
         SSB_df.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-SSB-run-%s.tsv'%(sub,run)), header=False, index=False, sep='\t')
         ## USB
         USB_df=taste_df.loc[taste_df['metric'] == '2']
+        #### check dataframe
+        c=taste_df.loc[taste_df['metric'] == '2']
+        check_dict.update({'USB':c})
+        ####################
         USB_df['metric'].replace({'2': 1}, inplace=True)
         USB_df.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-USB-run-%s.tsv'%(sub,run)), header=False, index=False, sep='\t')
         ## RINSE
@@ -113,11 +188,17 @@ def parsely(arglist):
             })
         rinse_df['onsets']=rinse_df['onsets'].astype('float32')-start_time
         rinse_df['duration']=3
+        check_dict.update({'rinse':rinse_df})
         rinse_df['metric']=1
         rinse_df.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-rinse-run-%s.tsv'%(sub,run)), header=False, index=False, sep='\t')
 
-
-        pdb.set_trace()
+        check_dict['rinse']['metric']='rinse'
+        DF=pd.concat(check_dict.values()).sort_values('onsets')
+        DF.to_csv(os.path.join(arglist['OUTPATH'],'%s'%session,'sub-%s_task-ALL-run-%s.csv'%(sub,run)))
+        if arglist['PLOT'] == True:
+            plottin(DF, arglist)
+        # DF=DF.sort_values('onsets')
+        # pdb.set_trace()
 
 
 def main():
@@ -129,6 +210,8 @@ def main():
                         default=False, help='Waves don die, which we using?')
     parser.add_argument('-outpath', dest='OUTPATH', action='store',
                         default=False, help='Where to save these toasty toasts')
+    parser.add_argument('-plot', dest='PLOT', action='store',
+                        default=False, help='You want a plot?')
 
     args = parser.parse_args()
     arglist={}
